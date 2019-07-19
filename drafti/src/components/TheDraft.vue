ef5b58fab4cffd7573751fff5605e0eb5a17f7d2
Alex Prascak <aprascak@ufl.edu>
2019-07-19 15:45:37 -0400

    Updated login page UI

--- drafti/src/components/TheDraft.vue
+++ drafti/src/components/TheDraft.vue
@@ -1,6 +1,19 @@
 <template id="">
-  <div id="the-draft">
-    <h4>The Draft</h4>
+  <div class="my-container">
+    <div class="content">
+      <h1><router-link class="white-text" :to="{ name: 'Landing' }">Drafti</router-link></h1>
+      <div class="divider"></div>
+      <div id="navrow" class="row">
+        <span><router-link class="white-text" :to="{ name: 'Landing' }">Mock Draft</router-link></span>
+        <span><router-link class="white-text" :to="{ name: 'Landing' }">Statistics</router-link></span>
+        <span><router-link class="white-text" :to="{ name: 'Landing' }">More</router-link></span>
+        <span><router-link class="white-text" :to="{ name: 'Landing' }">Profile</router-link></span>
+      </div>
+      <div class="row">
+        
+      </div>
+    </div>
+    <img src="../assets/Background-PaintedField.jpg">
   </div>
 </template>
 
@@ -11,12 +24,51 @@
       return {
 
       }
+    },
+    mounted() {
+      M.AutoInit()
     }
   }
 </script>
 
-<style media="screen">
-  #the-draft {
-    color: green;
+<style scoped>
+  .my-container {
+    position: relative;
+    overflow: hidden;
+    height: 100vh;
+  }
+  .my-container h1 {
+      position: relative;
+      z-index: 2;
+      margin-bottom:0%;
+  }
+  .my-container h5, h4, span {
+    position: relative;
+    z-index: 2;
+  }
+  .my-container img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0.75;
+  }
+  .my-container .divider {
+    position: relative;
+    z-index: 2;
+    width: 50%;
+    margin-left: 25%;
+    margin-top:0%;
+    height: 3px;
+    background-color: black;
+  }
+  .my-container span {
+    font-size: 18px;
+    margin-left: 1%;
+    margin-right: 1%;
+  }
+  #navrow {
+    margin-top: 0.5%;
   }
 </style>
--- drafti/src/components/auth/Login.vue
+++ drafti/src/components/auth/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="center container login">
+  <!-- <div class="center container login">
     <form @submit.prevent="login" class="card-panel hoverable">
       <h2 class="center blue-grey-text darken-4">Login</h2>
       <div class="field">
@@ -15,7 +15,38 @@
         <button class="btn deep-purple">Login</button>
       </div>
     </form>
+  </div> -->
+  <div class="my-container">
+    <h1 class="white-text center">Drafti</h1>
+    <div class="divider"></div>
+    <div id="navrow" class="row">
+      <span><router-link class="white-text" :to="{ name: 'TheDraft' }">Mock Draft</router-link></span>
+      <span><router-link class="white-text" :to="{ name: 'Landing' }">Statistics</router-link></span>
+      <span><router-link class="white-text" :to="{ name: 'Landing' }">More</router-link></span>
+      <span><router-link class="white-text" :to="{ name: 'Landing' }">Profile</router-link></span>
+    </div>
+    <div class="row">
+      <div class="col m4 l4 offset-m4 offset-l4">
+        <form @submit.prevent="login" class="card-panel hoverable center">
+          <h2 class="center black-text darken-4">Login</h2>
+          <div class="field">
+            <label for="email" class="left black-text">Email:</label>
+            <input type="email" name="email" v-model="email">
+          </div>
+          <div class="field">
+            <label for="email" class="left black-text">Password:</label>
+            <input type="password" name="password" v-model="password">
+          </div>
+          <p class="center red-text" v-if="feedback">{{ feedback }}</p>
+          <div class="field pulse">
+            <button class="btn black">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <img src="../../assets/Background-PaintedField.jpg">
   </div>
+
 </template>
 
 <script type="text/javascript">
@@ -51,12 +82,49 @@
   }
 </script>
 
-<style>
-  .login {
-    max-width: 550px;
-    margin-top: 60px;
+<style scoped>
+  .my-container {
+    position: relative;
+    overflow: hidden;
+    height: 100vh;
   }
-  .login .field {
-    margin-bottom: 20px;
+  .my-container h1 {
+      position: relative;
+      z-index: 2;
+      margin-bottom: 0px;
   }
+  .my-container h5, h4, span {
+    position: relative;
+    z-index: 2;
+  }
+  .my-container img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0.75;
+  }
+  .my-container .divider {
+    position: relative;
+    z-index: 2;
+    width: 33%;
+    margin-left: 33.3%;
+    margin-bottom: 1%;
+    height: 3px;
+    background-color: black;
+  }
+  form {
+    position: relative;
+    z-index: 2;
+  }
+  .field {
+    margin-bottom: 1%;
+  }
+  span {
+    font-size: 18px;
+    margin-left: 1%;
+    margin-right: 1%;
+  }
+
 </style>
